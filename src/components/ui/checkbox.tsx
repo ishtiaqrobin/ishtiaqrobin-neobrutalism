06a8da6d49ffd2405a5b5fe6e58cafe31e486503
Ishtiaq Robin <robin.pass36@gmail.com>
2026-08-08 23:26:26 +0600

    feat: add custom Checkbox component using Radix UI and Lucide icons

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,16 +14,16 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer size-4.5 shrink-0 rounded-md border-2 border-black dark:border-zinc-300 shadow-[1.5px_1.5px_0px_0px_#000] dark:shadow-[1.5px_1.5px_0px_0px_#b5ff6d] bg-white dark:bg-zinc-900 data-[state=checked]:bg-[#b5ff6d] data-[state=checked]:text-black cursor-pointer transition-all outline-none disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
-        className="grid place-content-center text-current transition-none"
+        className="grid place-content-center text-black font-black transition-none"
       >
-        <CheckIcon className="size-3.5" />
+        <CheckIcon className="size-3.5 stroke-[3]" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   )
